5d9c90f18d1210e5d92447a6a1661a23f8e2942c
Kate Bakhtina <almazniegori@gmail.com>
2025-01-18 23:27:03 +0300

    added_test_for_full_coverage

--- src/test/java/ru/netology/statistic/service/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/service/StatisticServiceTest.java
@@ -15,4 +15,16 @@ public class StatisticServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWithFullCoverage() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 12, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
